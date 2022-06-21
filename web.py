696f4795b4fd5b19f91718cba8e9cfed3b954269
nyitrairicsi99 <58612330+nyitrairicsi99@users.noreply.github.com>
2022-06-21 02:22:49 +0200

    Added some css

--- web.py
+++ web.py
@@ -33,7 +33,7 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
-            self.wfile.write(bytes("<html><head><title>Profession.hu állások</title></head>", "utf-8"))
+            self.wfile.write(bytes("<html><head><title>Profession.hu állások</title><style>div.container,p {width: 300px; float:left; height: 150px; background-color: lightgrey; margin: 5px}</style></head>", "utf-8"))
             self.wfile.write(bytes("""
             <script>
                 function deleterow(id) {
@@ -50,11 +50,11 @@ class MyServer(BaseHTTPRequestHandler):
             """, "utf-8"))
             self.wfile.write(bytes("<body>", "utf-8"))
             file = open("lastjobs.txt",encoding='utf-8')
-            for row in file:
+            for row in reversed(file.readlines()):
                 row = row.strip().split(" | ")
                 if len(row)>1: #nem elválasztó
                     if row[1]=="0":
-                        self.wfile.write(bytes("<p><a href='"+row[5]+"' target='_blank'><strong>"+row[2]+"</strong></a><br />"+row[3]+"<br />"+row[4]+"<br /><i style='cursor: pointer;' onclick=deleterow("+(row[0])+")>Törlés</i></p>", "utf-8"))
+                        self.wfile.write(bytes("<div class='container'><p><a href='"+row[5]+"' target='_blank'><strong>"+row[2]+"</strong></a><br />"+row[3]+"<br />"+row[4]+"<br /><i style='cursor: pointer;' onclick=deleterow("+(row[0])+")>Törlés</i></p></div>", "utf-8"))
             file.close()
 
             
